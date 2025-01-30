65c1aafa49591702dbb59121a5fb7f928e026255
Braien Machado de Paula <braienmp@outlook.com>
2025-01-30 20:57:56 -0300

    feat: create zombiehelper contract and allow getting zombie ids owned by address

---
+++ contracts/ZombieHelper.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import './ZombieFeeding.sol';
+
+contract ZombieHelper is ZombieFeeding {
+  modifier aboveLevel(uint _level, uint _zombieId) {
+    require(zombies[_zombieId - 1].level >= _level);
+    _;
+  }
+
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId - 1].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId - 1].dna = _newDna;
+  }
+
+  function getZombiesByOwner(address _owner) external view returns (uint[] memory) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    uint counter = 0;
+    for (uint i = 1; i <= zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
+  }
+}
